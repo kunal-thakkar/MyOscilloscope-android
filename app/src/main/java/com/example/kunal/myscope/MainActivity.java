99c7b1e11383157cfc1dc74d14b27a1c98462629
Kunal-Home_Laptop <kunalmthakkar@gmail.com>
2017-10-09 22:38:25 +0530

    Solved USB communication with digiUSB

--- app/src/main/java/com/example/kunal/myscope/MainActivity.java
+++ app/src/main/java/com/example/kunal/myscope/MainActivity.java
@@ -12,6 +12,8 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -21,44 +23,32 @@ import java.util.Iterator;
 
 public class MainActivity extends Activity {
 
-    private byte[] bytes;
-    private static int TIMEOUT = 0;
-    private boolean forceClaim = true;
-
-    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
-
-    int Vendor = 0x16c0;
-    int productId = 0x05df;
-
     private static final String TAG = "MyScope";
-    private UsbDeviceConnection usbDeviceConnection;
-    private UsbInterface usbInterface;
-    private boolean deviceFound = false;
+
+    public static final int REQ_OUT = UsbConstants.USB_TYPE_CLASS + UsbConstants.USB_DIR_OUT;
+    public static final int REQ_IN = UsbConstants.USB_TYPE_CLASS + UsbConstants.USB_DIR_IN;
+
+    private CalibrationTask mCalibTask;
+    private static final int DIGISPARK_VID = 0x16C0;
+    private static final int DIGISPARK_PID = 0x05DF;
+    private static final int REQ_USB_PERMISSION = 1;
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent != null && intent.getData() != null && "usb".equals(intent.getData().getScheme())) {
+            UsbDevice device = intent.getParcelableExtra("device");
+            if (device != null) {
+                onDeviceSelected(device);
+            }
+        }
+    }
 
     @Override
     public void onPause() {
         super.onPause();  // Always call the superclass method first
         Log.i(TAG, "releaseUsb()");
-        if (usbDeviceConnection != null) {
-            if (usbInterface != null) {
-                usbDeviceConnection.releaseInterface(usbInterface); usbInterface = null;
-            }
-            usbDeviceConnection.close();
-            usbDeviceConnection = null;
-        }
-    }
-
-    byte[] toByteArray(int value) {
-        return new byte[] {
-                (byte)(value >> 24),
-                (byte)(value >> 16),
-                (byte)(value >> 8),
-                (byte)value };
-    }
-
-    int fromByteArray(byte[] bytes) {
-        //return bytes[0] << 24 | (bytes[1] & 0xFF) << 16;// | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
-        return bytes[0] & 0xFF;
+        if(mCalibTask != null && !mCalibTask.isCancelled()) mCalibTask.cancel(true);
     }
 
     @Override
@@ -73,65 +63,59 @@ public class MainActivity extends Activity {
         if(device == null) {
             while (deviceIterator.hasNext()) {
                 device = deviceIterator.next();
-                if (device.getProductId() == productId && device.getVendorId() == Vendor) {
-                    deviceFound = true;
+                if (device.getProductId() == DIGISPARK_PID && device.getVendorId() == DIGISPARK_VID) {
+                    Intent in = new Intent(MainActivity.this, MainActivity.class);
+                    Uri u = new Uri.Builder().scheme("usb").path(device.getDeviceName()).build();
+                    in.setData(u);
+                    in.putExtra("device", device);  // Parcel
+                    PendingIntent pi = PendingIntent.getActivity(MainActivity.this, REQ_USB_PERMISSION, in, PendingIntent.FLAG_UPDATE_CURRENT);
+                    manager.requestPermission(device, pi);
                     break;
                 }
             }
         }
         else {
-            deviceFound = true;
+            onDeviceSelected(device);
         }
-        if(!deviceFound){
-            Log.i(TAG, "device not found");
-        }
-        else {
-            Log.i(TAG, "device found");
-            Log.i(TAG, "Name: " + device.getDeviceName());
-            Log.i(TAG, "ID: " + device.getDeviceId());
-            Log.i(TAG, "Protocol: " + device.getDeviceProtocol());
-            Log.i(TAG, "Class: " + device.getDeviceClass());
-            Log.i(TAG, "Subclass: " + device.getDeviceSubclass());
-            Log.i(TAG, "Product ID: " + device.getProductId());
-            Log.i(TAG, "Vendor ID: " + device.getVendorId());
-            Log.i(TAG, "Interface count: " + device.getInterfaceCount());
+    }
 
-            usbInterface = device.getInterface(0);
-            UsbEndpoint tOut = null;
-            UsbEndpoint tIn = null;
-            int tEndpointCnt = usbInterface.getEndpointCount();
-            Log.i(TAG, "End Point Count : " + tEndpointCnt);
-            for (int j = 0; j < tEndpointCnt; j++) {
-                Log.i(TAG, j + " : " + usbInterface.getEndpoint(j).getDirection());
-                if (usbInterface.getEndpoint(j).getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
-                    if (usbInterface.getEndpoint(j).getDirection() == UsbConstants.USB_DIR_OUT) {
-                        tOut = usbInterface.getEndpoint(j);
-                    } else if (usbInterface.getEndpoint(j).getDirection() == UsbConstants.USB_DIR_IN) {
-                        tIn = usbInterface.getEndpoint(j);
-                    }
-                }
+    //https://digistump.com/board/index.php?topic=1675.0
+    class CalibrationTask extends AsyncTask<UsbDevice, Integer, Void>{
+        @Override
+        protected Void doInBackground(UsbDevice... params) {
+            UsbDevice device = params[0];
+            UsbManager manager = (UsbManager) MainActivity.this.getSystemService(Context.USB_SERVICE);
+            UsbDeviceConnection cnx = manager.openDevice(device);
+            // handle openDevice failure as needed for your application
+            byte buffer[] = new byte[2];
+            while (!isCancelled()) {
+                //int requesttype, int request, int value, int index, char *bytes, int size, int timeout
+                cnx.controlTransfer(REQ_IN, 0x01, 0x00, 0x00, buffer, 2, 1000);
+                publishProgress((buffer[1] & 0xFF) << 8 | (buffer[0] & 0xFF));
+                cnx.controlTransfer(REQ_OUT, 0x09, 0x00, 0x01, null, 0, 1000);
             }
-//            if(tOut != null && tIn != null){
-//                Log.i(TAG, "Found OK");
-                bytes = new byte[1];
-                UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-                usbDeviceConnection = mUsbManager.openDevice(device);
-
-                int i = 0, t;
-                while(i < 100) {
-                    byte buffer[] = new byte[1];
-                    //connection.bulkTransfer(tIn, buffer, 0x01, 0);
-                    //requestType, request, value, index, buffer, length, timeout
-                    t = usbDeviceConnection.controlTransfer(UsbConstants.USB_DIR_IN, 0x03 << 8, 0x01, 0x00, buffer, 1, 1000); //0x27 0x00
-                    Log.i(TAG, "Value : " + fromByteArray(buffer) + ", " + t);
-                    //connection.bulkTransfer(tOut, null, 0x01, 0);
-                    t = usbDeviceConnection.controlTransfer(UsbConstants.USB_DIR_OUT, 0x09, 0x03 << 8, 0x01, null, 0, 1000);
-                    Log.i(TAG, "Value : " + fromByteArray(buffer) + ", " + t);
-                    i++;
-                }
-                //connection.bulkTransfer(endpoint, bytes, bytes.length, TIMEOUT); //do in another thread
-//            }
+            cnx.close();
+            return null;
         }
 
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.i(TAG, "Value : "+ values[0]);
+        }
+    }
+
+    private void onDeviceSelected(UsbDevice device) {
+        Log.i(TAG, "device found");
+        Log.i(TAG, "Name: " + device.getDeviceName());
+        Log.i(TAG, "ID: " + device.getDeviceId());
+        Log.i(TAG, "Protocol: " + device.getDeviceProtocol());
+        Log.i(TAG, "Class: " + device.getDeviceClass());
+        Log.i(TAG, "Subclass: " + device.getDeviceSubclass());
+        Log.i(TAG, "Product ID: " + device.getProductId());
+        Log.i(TAG, "Vendor ID: " + device.getVendorId());
+        Log.i(TAG, "Interface count: " + device.getInterfaceCount());
+        mCalibTask = new CalibrationTask();
+        mCalibTask.execute(device);
     }
 }
